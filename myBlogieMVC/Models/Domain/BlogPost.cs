2ef90a93879dc3b2dfdeaed93b39dab5793974b5
SimonB87 <buryans@gmail.com>
2024-04-17 23:32:56 +0200

    Domin models

---
+++ myBlogieMVC/Models/Domain/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace myBlogieMVC.Models.Domain
+{
+	public class BlogPost
+	{
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+		public string PageTitle { get; set; }
+		public string Content { get; set; }
+
+	}
+}
