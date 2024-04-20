05dd1e56c696598c7d660cfbf0ebcc6a79dc78bc
SimonB87 <buryans@gmail.com>
2024-04-20 15:11:46 +0200

    Domain model - BlogPosts finished

--- myBlogieMVC/Models/Domain/BlogPost.cs
+++ myBlogieMVC/Models/Domain/BlogPost.cs
@@ -7,5 +7,19 @@
 		public string PageTitle { get; set; }
 		public string Content { get; set; }
 
-	}
+		public string ShortDescription { get; set; }
+
+		public string FeaturedImageUrl { get; set; }
+
+		public string UrlHandle { get; set; }
+
+		public DateTime PublishedDate { get; set; }
+
+		public string Author { get; set; }
+
+		public bool Visible { get; set; }
+
+        public ICollection<Tag> Tag { get; set; }
+
+    }
 }
