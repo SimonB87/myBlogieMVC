4508c5c9c115e23642c418ddb599b8753fd523bd
SimonB87 <buryans@gmail.com>
2024-06-18 23:33:34 +0200

    Added Add Tag page

---
+++ myBloggieMVC/Views/AdminTags/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myBloggieMVC.Views.AdminTags
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
