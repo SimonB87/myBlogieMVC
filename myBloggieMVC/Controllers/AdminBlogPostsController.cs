ff69f55c9f5cd0cd96b4fcdd4be7177b5453acad
SimonB87 <buryans@gmail.com>
2024-07-29 23:45:28 +0200

    Add AdminBlogPostsController

---
+++ myBloggieMVC/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myBloggieMVC.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            return View();
+        }
+    }
+}
